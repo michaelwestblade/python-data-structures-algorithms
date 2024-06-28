2448038f92ce6737454355f7876d8df5fb83272c
michaelwestblade <michaelwestblade@gmail.com>
2024-06-27 19:31:15 -0500

    has loop method

---
+++ linked_list/has_loop.py
@@ -0,0 +1,53 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+
+    def has_loop(self):
+        slow = self.head
+        fast = self.head
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
+    def has_loop_practice(self):
+        if self.head.next is None:
+            return False
+
+        slow = self.head
+        fast = self.head.next
+
+        while fast is not None and fast.next is not None and slow != fast:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow == fast
+
+my_linked_list_1 = LinkedList(1)
+my_linked_list_1.append(2)
+my_linked_list_1.append(3)
+my_linked_list_1.append(4)
+my_linked_list_1.tail.next = my_linked_list_1.head
+print(my_linked_list_1.has_loop())  # Returns True
