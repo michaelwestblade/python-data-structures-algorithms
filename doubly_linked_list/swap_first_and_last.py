54036e3e1768a2576473315623a1f465e66c6f0c
michaelwestblade <michaelwestblade@gmail.com>
2024-06-28 13:09:14 -0500

    dll challenges

---
+++ doubly_linked_list/swap_first_and_last.py
@@ -0,0 +1,60 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def swap_first_last_Practice(self):
+        if self.length == 0 or self.length == 1:
+            return False
+
+        first_value = self.head.value
+        last_value = self.tail.value
+        self.head.value = last_value
+        self.tail.value = first_value
+
+    def swap_first_last(self):
+        if self.head is None or self.head == self.tail:
+            return
+        self.head.value, self.tail.value = self.tail.value, self.head.value
+
+my_doubly_linked_list = DoublyLinkedList(1)
+my_doubly_linked_list.append(2)
+my_doubly_linked_list.append(3)
+my_doubly_linked_list.append(4)
+
+print('DLL before swap_first_last():')
+my_doubly_linked_list.print_list()
+
+
+my_doubly_linked_list.swap_first_last()
+
+
+print('\nDLL after swap_first_last():')
+my_doubly_linked_list.print_list()
---
+++ doubly_linked_list/swap_nodes_in_pairs.py
@@ -0,0 +1,113 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.length = 1
+
+    def print_list(self):
+        output = []
+        current_node = self.head
+        while current_node is not None:
+            output.append(str(current_node.value))
+            current_node = current_node.next
+        print(" <-> ".join(output))
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+        else:
+            temp = self.head
+            while temp.next is not None:
+                temp = temp.next
+            temp.next = new_node
+            new_node.prev = temp
+        self.length += 1
+        return True
+
+    def swap_pairs_practice(self):
+        if self.length == 0 or self.length == 1:
+            return False
+
+        current_node = self.head.next
+        while current_node is not None:
+            first = current_node.prev
+            second = current_node
+            preceding_node = first.prev
+            trailing_node = second.next
+
+            if first == self.head:
+                self.head = second
+
+            second.prev, second.next, first.next, first.prev = first.prev, first, second.next, second
+
+            if preceding_node is not None:
+                preceding_node.next = second
+
+            if trailing_node is not None:
+                trailing_node.prev = first
+
+            if first.next is None:
+                return False
+            current_node = first.next.next
+
+    def swap_pairs(self):
+        dummy_node = Node(0)
+        dummy_node.next = self.head
+        previous_node = dummy_node
+
+        while self.head and self.head.next:
+            first_node = self.head
+            second_node = self.head.next
+
+            previous_node.next = second_node
+            first_node.next = second_node.next
+            second_node.next = first_node
+
+            second_node.prev = previous_node
+            first_node.prev = second_node
+
+            if first_node.next:
+                first_node.next.prev = first_node
+
+            self.head = first_node.next
+            previous_node = first_node
+
+        self.head = dummy_node.next
+
+        if self.head:
+            self.head.prev = None
+
+
+my_dll = DoublyLinkedList(1)
+my_dll.append(2)
+my_dll.append(3)
+my_dll.append(4)
+
+print('my_dll before swap_pairs:')
+my_dll.print_list()
+
+my_dll.swap_pairs()
+
+
+print('my_dll after swap_pairs:')
+my_dll.print_list()
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    my_dll before swap_pairs:
+    1 <-> 2 <-> 3 <-> 4
+    ------------------------
+    my_dll after swap_pairs:
+    2 <-> 1 <-> 4 <-> 3
+
+"""
