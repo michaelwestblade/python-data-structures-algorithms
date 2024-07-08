0eacf893eeaa065c5a277a36f01582185eba1559
michaelwestblade <michaelwestblade@gmail.com>
2024-07-07 21:31:53 -0500

    fibonacci done bottom up instead of recursive

--- dynamic_programming/fibonacci_sequence.py
+++ dynamic_programming/fibonacci_sequence.py
@@ -27,6 +27,20 @@ def fib_optimized(n: int) -> Union[Optional[int], None]:
     memo[n] = fib_optimized(n - 1) + fib_optimized(n - 2)
     return memo[n]
 
+bottom_up_counter = 0
+
+def fib_bottom_up(n: int) -> int:
+    global bottom_up_counter
+
+    fib_list = [0, 1]
+
+    for index in range(2, n + 1):
+        bottom_up_counter += 1
+        next_fib = fib_list[index - 1] + fib_list[index - 2]
+        fib_list.append(next_fib)
+
+    return fib_list[n]
+
 
 n = 30
 print(f"\nFib of {n} is {fib(n)}")
@@ -37,3 +51,6 @@ print(f"\nFib of {n} is {fib_optimized(n)}")
 print(f"\nCounter: {optimized_counter}")
 
 print("".join([f"{str(x)}, " for x in memo if x is not None]))
+
+print(f"\nFib of {n} is {fib_bottom_up(n)}")
+print(f"\nCounter: {bottom_up_counter}")
